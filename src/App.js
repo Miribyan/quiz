8410bcbf1b55536f74e9ca33e5a4bd05fadac059
Marat Miribyan <122740363+Miribyan@users.noreply.github.com>
2023-03-04 03:54:31 +0400

    add time limit in each question, update nextQuestion function, add timer function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import questions from "./questions";
 import "./index.css";
 
 
+
+
 function Quiz() {
-  const [currentQuestion, setCurrentQuestion] = useState(9);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
   const [selectedAnswers, setSelectedAnswers] = useState([]);
   const [isComplete, setIsComplete] = useState(false);
   const [score, setScore] = useState(0);
+  const [Limit, setLimit] = useState(questions[currentQuestion].timeLimit)
 
   function handleAnswerChange(option) {
     const newSelectedAnswers = [...selectedAnswers];
@@ -18,7 +21,7 @@ function Quiz() {
 
 
 
-  function handleNextQuestionClick() {
+  function handleNextQuestion() {
     const currentAnswer = selectedAnswers[currentQuestion];
     if (currentAnswer !== undefined) {
       const currentScore = questions[currentQuestion].answers.find(
@@ -30,8 +33,27 @@ function Quiz() {
       if (questions.length > currentQuestion + 1) {
         setCurrentQuestion(currentQuestion + 1)
       } else { setIsComplete(true) };
-    }
+    } else { setScore(score) ;
+      if (questions.length > currentQuestion + 1) {
+        setCurrentQuestion(currentQuestion + 1)
+      } else { setIsComplete(true) }; }
   }
+
+
+  useEffect(() => {
+    const Timer =
+      Limit > 0 &&
+      setInterval(function () {
+        setLimit(Limit - 1)
+      }, 1000);
+    if (Limit === 0) { handleNextQuestion() }
+    return () => clearInterval(Timer)
+  }, [Limit])
+
+  useEffect (() => {
+    setLimit(questions[currentQuestion].timeLimit);
+  }, [currentQuestion])
+
 
 
   return (
@@ -61,8 +83,9 @@ function Quiz() {
               </li>
             ))}
           </ul>
-          <button onClick={handleNextQuestionClick}>Next question</button>
+          <button onClick={handleNextQuestion}>Next question</button>
         </div>
+        <div>{Limit}</div>
       </div>
     ));
 }
--- src/questions.js
+++ src/questions.js
@@ -9,6 +9,7 @@ const questions = [
           { option: "Не изменяется", isCorrect: false },
           { option: "Не иследовано", isCorrect: false }
         ],
+        timeLimit: 5
       }, {
         question: "Мышечный тонус при поражении центрального двигательного нейрона: ",
         round: "Вопросы по Медицине",
@@ -18,6 +19,7 @@ const questions = [
           { option: "Не изменяется", isCorrect: false },
           { option: "Не иследовано", isCorrect: false }
         ],
+        timeLimit: 5
       }, {
         question: "Патологические пирамидные симптомы на верхней конечности - рефлексы: ",
         round: "Вопросы по Медицине",
@@ -27,6 +29,7 @@ const questions = [
           { option: "Россолимо", isCorrect: true },
           { option: "Шеффера", isCorrect: false }
         ],
+        timeLimit: 5
       }, {
         question: "Гипотрофия мышц характерна для поражения: ",
         round: "Вопросы по Медицине",
@@ -36,6 +39,7 @@ const questions = [
           { option: "Мозжечка", isCorrect: false },
           { option: "Гипоталамуса", isCorrect: false }
         ],
+        timeLimit: 5
       }, {
         question: "При поражении отводящего нерва возникает паралич глазодвигательной мышцы: ",
         round: "Вопросы по Медицине",
@@ -45,6 +49,7 @@ const questions = [
           { option: "Нижней прямой", isCorrect: false },
           { option: "Нижней косой", isCorrect: false }
         ],
+        timeLimit: 5
       }, {
         question: "При центральном параличе наблюдается:",
         round: "Вопросы по Медицине",
@@ -54,6 +59,7 @@ const questions = [
           { option: "Фибрилярные подергивания", isCorrect: false },
           { option: "Не иследовано", isCorrect: false }
         ],
+        timeLimit: 5
       }, {
         question: "Хореический гиперкинез возникает при поражении: ",
         round: "Вопросы по Медицине",
@@ -63,6 +69,7 @@ const questions = [
           { option: "Бедиального бледного шара", isCorrect: false },
           { option: "Латерального бледного шара", isCorrect: false }
         ],
+        timeLimit: 5
       }, {
         question: "Основным медиатором тормозного действия является: ",
         round: "Вопросы по Медицине",
@@ -72,6 +79,7 @@ const questions = [
           { option: "Норадреналин", isCorrect: false },
           { option: "Адреналин", isCorrect: false }
         ],
+        timeLimit: 5
       }, {
         question: "Все афферентные пути стриопаллидарной системы оканчиваются:",
         round: "Вопросы по Медицине",
@@ -81,6 +89,7 @@ const questions = [
           { option: "В медиальном ядре бледного шара", isCorrect: false },
           { option: "В субталамическом ядре", isCorrect: false }
         ],
+        timeLimit: 5
       }, {
         question: "Обонятельные галлюцинации наблюдаются при поражении: ",
         round: "Вопросы по Медицине",
@@ -90,6 +99,7 @@ const questions = [
           { option: "Височной доли", isCorrect: true },
           { option: "Теменной доли", isCorrect: false }
         ],
+        timeLimit: 5
       }
       //  {
     //     question: "Столица Германии?",
